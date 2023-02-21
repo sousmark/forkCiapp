8dea84295d4bc074ed6a9fcb4242738cd80be0cd
boufenny <yassine.boufenneche@esme.fr>
2023-02-21 13:54:55 +0100

    first commit

---
+++ src/main/java/fr/esme/apprentis/CiappApplication.java
@@ -0,0 +1,13 @@
+package fr.esme.apprentis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CiappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CiappApplication.class, args);
+	}
+
+}
---
+++ src/test/java/fr/esme/apprentis/CiappApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.esme.apprentis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CiappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
